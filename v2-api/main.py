f2253b05a1f03d90c2ed68560066539c8933e801
Crafter-deo <adesolaadeoluwa@gmail.com>
2022-04-29 23:00:48 +0100

    added endpoints info to JSON

--- v2-api/main.py
+++ v2-api/main.py
@@ -7,6 +7,9 @@ app = FastAPI()
 
 @app.get('/')
 def home():
-    return {'Tech News': 'Get top trending news via this API'}
+    endpoints = {"combined news": "/news", "cnet": "/news/cnet", "mashable": "/news/mashable",
+                 "medium": "/news/medium", "the next web": "/news/tnw", "wired": "news/wired"}
+    return {'Tech News': 'Get top trending news via this API', 'endpoints': endpoints}
+
 
 app.include_router(news.router)
